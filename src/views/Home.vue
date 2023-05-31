f112e7d9bcbffcd1ef7c2f12049c8c039e64c867
李超越 <lcyue@localhost.com>
2023-05-31 10:36:03 +0800

    chore: 项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description: 
+ * @Author: 李超越
+ * @Date: 2023-05-31 09:33:42
+ * @LastEditTime: 2023-05-31 09:47:56
+ * @LastEditors: 李超越
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "BaseHome",
+  components: {},
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Description: 
+ * @Author: 李超越
+ * @Date: 2023-05-31 09:33:42
+ * @LastEditTime: 2023-05-31 09:48:07
+ * @LastEditors: 李超越
+-->
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseLogin",
+  components: {},
+};
+</script>
